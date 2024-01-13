c0d5f01af3dede61aefce2e8548d6fc80b557a67
Frederick De Baene <frederick.debaene@hotmail.com>
2024-01-13 17:49:07 +0100

    Define methods for stats

--- src/costly/statements/statement.py
+++ src/costly/statements/statement.py
@@ -33,3 +33,31 @@ class AccountStatement:
             end_date=data["end_date"],
             data=data["data"]
         )
+    
+    def sum_transactions(self) -> float:
+        """
+        This method sums the transactions in the given statement, both 
+        incoming and outgoing transactions.
+
+        Returns:
+            float: The summed transactions.
+        """
+        return self.data["amount"].sum()
+    
+    def sum_incoming(self) -> float:
+        """
+        This method sums the incoming transactions in the statement.
+
+        Returns:
+            float: The summed incoming transactions.
+        """
+        return self.data["credit"].sum()
+    
+    def sum_outgoing(self) -> float:
+        """
+        This method sums the outgoing transactions in the statement.
+
+        Returns:
+            float: The summed outgoing transactions.
+        """
+        return self.data["debit"].sum()
